8c270b8e0b22333ecf3e3296319fcc9843c53853
Andrey <MrAbhorrent@yandex.ru>
2021-12-25 23:05:33 +0500

    Homework for lesson 01

--- app/src/main/java/ru/gb/kotlin/firstApp/MainActivity.kt
+++ app/src/main/java/ru/gb/kotlin/firstApp/MainActivity.kt
@@ -3,8 +3,12 @@ package ru.gb.kotlin.firstApp
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import java.util.Spliterators.iterator
+import ru.gb.kotlin.firstApp.Weather as Weather
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,20 +32,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.btnNewText)
+        val button1 = findViewById<Button>(R.id.btnNewText)
         val textView = findViewById<TextView>(R.id.tvWelcome)
-        button.setOnClickListener {
+        val button2 = findViewById<Button>(R.id.btnWeather)
+        var button3 = findViewById<Button>(R.id.btnCopy)
+
+        button1.setOnClickListener {
             if (textView.text.equals(tvTextRus)) {
                 textView.text = tvTextEng
             } else {
                 textView.text = tvTextRus
             }
-            if (button.text.equals(buttonTextRus)) {
-                button.text = buttonTextEng
+            if (button1.text.equals(buttonTextRus)) {
+                button1.text = buttonTextEng
             } else {
-                button.text = buttonTextRus
+                button1.text = buttonTextRus
             }
-
         }
+
+        button2.setOnClickListener {
+            val weather = Weather("Омск", -4)
+            Snackbar.make(this.findViewById(R.id.activity_main), "City : ${weather.town}, Temperature : ${weather.temperature}", Snackbar.LENGTH_SHORT).show()
+        }
+
+        button3.setOnClickListener {
+            val DefaultCityWeather = Singleton.getMoskowWeather()
+            val AnotherCopyWeather = DefaultCityWeather.copy()
+
+            Snackbar.make(this.findViewById(R.id.activity_main), "Copy weather town : ${AnotherCopyWeather.town}, temp = ${AnotherCopyWeather.temperature}", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
+
+object Singleton {
+    private val MoskowWeather: Weather = Weather()
+
+    fun  getMoskowWeather(): Weather {
+        return Weather()
     }
 }
---
+++ app/src/main/java/ru/gb/kotlin/firstApp/Weather.kt
@@ -0,0 +1,5 @@
+package ru.gb.kotlin.firstApp
+
+data class Weather (
+    val town: String = "Москва",
+    val temperature: Int = 0)
